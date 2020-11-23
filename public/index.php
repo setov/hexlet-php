b41a7b07dda3e01dec463c8a9eec593e7bc08539
Sergey Titov <ya.sergeTitov@yandex.ru>
2020-11-23 05:11:31 +0300

    template for practice

---
+++ public/index.php
@@ -0,0 +1,41 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Instantiate App
+$app = AppFactory::create();
+
+// Add error middleware
+$app->addErrorMiddleware(true, true, true);
+
+// Add routes
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('<a href="/hello/world">Try /hello/world</a>');
+    return $response;
+});
+
+$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+$app->get('/about', function ($request, $response) {
+    $phpView = new PhpRenderer('../templates');
+    return $phpView->render($response, 'about.phtml');
+});
+
+$app->run();
+
+/**
+ * You may quickly test this using the built-in PHP server:
+ * php -S localhost:8000 -t public
+ * Going to http://localhost:8000/hello/world will now display "Hello, world".
+ * http://localhost:8000/hello/sergey
+ * http://localhost:8000/about
+ */
